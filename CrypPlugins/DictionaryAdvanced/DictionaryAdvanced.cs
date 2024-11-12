4a9459283afcc78fbe405988b60e49ef793bed18
Plasmator-x64 <plasmator.x64@gmail.com>
2024-11-11 22:47:27 -0500

    Add TitleCase

---
+++ CrypPlugins/DictionaryAdvanced/DictionaryAdvanced.cs
@@ -0,0 +1,178 @@
+﻿/*
+   Copyright CrypTool 2 Team <ct2contact@cryptool.org>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using CrypTool.PluginBase;
+using CrypTool.PluginBase.IO;
+using CrypTool.PluginBase.Miscellaneous;
+using LanguageStatisticsLib;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Controls;
+using System.Threading;
+
+namespace CrypTool.Plugins.DictionaryAdvanced
+{
+
+    [Author("Plasmator", "coredevs@cryptool.org", "CrypTool 2 Team", "https://www.cryptool.org")]
+    [PluginInfo("DictionaryAdvanced.Properties.Resources", "PluginCaption", "PluginTooltip", "DictionaryAdvanced/DetailedDescription/doc.xml", "DictionaryAdvanced/icon.png")]
+    [ComponentCategory(ComponentCategory.ToolsDataInputOutput)]
+
+    public class DictionaryAdvanced : ICrypComponent
+    {
+        #region Private Variables
+
+        private readonly DictionaryAdvancedSettings _Settings = new DictionaryAdvancedSettings();
+        private readonly Dictionary<int, string[]> _DictionaryCache = new Dictionary<int, string[]>();
+        private readonly TextInfo _TextInfo = Thread.CurrentThread.CurrentCulture.TextInfo;
+
+        #endregion
+
+        #region Data Properties
+
+        [PropertyInfo(Direction.OutputData, "DictionaryArrayOutputCaption", "DictionaryArrayOutputTooltip", false)]
+        public string[] OutputList
+        {
+            get;
+            set;
+        }
+
+        [PropertyInfo(Direction.OutputData, "DictionaryLengthOutputCaption", "DictionaryLengthOutputTooltip", false)]
+        public int OutputLength
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region IPlugin Members
+
+        public ISettings Settings
+        {
+            get { return _Settings; }
+        }
+
+        public UserControl Presentation
+        {
+            get { return null; }
+        }
+
+        public void PreExecution()
+        {
+        }
+
+        /// <summary>
+        /// Excutes the Plugin - Output the Selected Dictionary as a String Array
+        /// </summary>
+        public void Execute()
+        {
+
+            ProgressChanged(0, 1);
+
+            if (!_DictionaryCache.ContainsKey(_Settings.Language))
+            {
+                List<string> _Dictionary = LanguageStatistics.LoadDictionary(LanguageStatistics.LanguageCode(_Settings.Language), DirectoryHelper.DirectoryLanguageStatistics);
+                _DictionaryCache.Add(_Settings.Language, _Dictionary.ToArray());
+            }
+
+            OutputList = _DictionaryCache[_Settings.Language].ToArray();
+            OutputLength = OutputList.Length;
+
+            switch (_Settings.CharacterCase)
+            {
+                case CharCase.Lowercase:
+                    OutputList = OutputList.Select(x => x.ToLower()).ToArray();
+                    break;
+
+                case CharCase.Titlecase:
+                    OutputList = OutputList.Select(x => _TextInfo.ToTitleCase(x.ToLower())).ToArray();
+                    break;
+
+                case CharCase.Uppercase:
+                    OutputList = OutputList.Select(x => x.ToUpper()).ToArray();
+                    break;
+
+                default:
+                    break;
+            }
+            switch (_Settings.CharacterDirection)
+            {
+                case CharDir.Forward:
+                    OutputList = OutputList;
+                    break;
+
+                case CharDir.Reverse:
+                    OutputList = OutputList.Select(x => new string(x.Reverse().ToArray())).ToArray();
+                    break;
+
+                default:
+                    break;
+            }
+
+            OnPropertyChanged(nameof(OutputList));
+            OnPropertyChanged(nameof(OutputLength));
+
+            ProgressChanged(1, 1);
+        }
+
+        public void PostExecution()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+
+        public void Initialize()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+
+        #endregion
+
+        #region Event Handling
+
+        public event StatusChangedEventHandler OnPluginStatusChanged;
+
+        public event GuiLogNotificationEventHandler OnGuiLogNotificationOccured;
+
+        public event PluginProgressChangedEventHandler OnPluginProgressChanged;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void GuiLogMessage(string message, NotificationLevel logLevel)
+        {
+            EventsHelper.GuiLogMessage(OnGuiLogNotificationOccured, this, new GuiLogEventArgs(message, this, logLevel));
+        }
+
+        private void OnPropertyChanged(string name)
+        {
+            EventsHelper.PropertyChanged(PropertyChanged, this, new PropertyChangedEventArgs(name));
+        }
+
+        private void ProgressChanged(double value, double max)
+        {
+            EventsHelper.ProgressChanged(OnPluginProgressChanged, this, new PluginProgressEventArgs(value, max));
+        }
+
+        #endregion
+    }
+}
---
+++ CrypPlugins/DictionaryAdvanced/DictionaryAdvancedSettings.cs
@@ -0,0 +1,103 @@
+﻿/*
+   Copyright CrypTool 2 Team <ct2contact@cryptool.org>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using CrypTool.PluginBase;
+using CrypTool.PluginBase.Miscellaneous;
+using LanguageStatisticsLib;
+using System.ComponentModel;
+
+namespace CrypTool.Plugins.DictionaryAdvanced
+{
+
+    public enum CharCase
+    {
+        Lowercase,
+        Titlecase,
+        Uppercase
+    }
+
+    public enum CharDir
+    {
+        Forward,
+        Reverse
+    }
+
+    public class DictionaryAdvancedSettings : ISettings
+    {
+        private string _LanguageCode = "en"; // Set Default Language to English
+        private CharCase _CharCase = CharCase.Uppercase; // Set Default Char Case to Upper
+        private CharDir _CharDir = CharDir.Forward; // Set Default Char Direction to Forward
+
+        [TaskPane("DictionaryCaption", "DictionaryTooltip", null, 0, false, ControlType.LanguageSelector)]
+        public int Language
+        {
+            get => LanguageStatistics.LanguageId(_LanguageCode);
+            set
+            {
+                if (value != LanguageStatistics.LanguageId(_LanguageCode))
+                {
+                    _LanguageCode = LanguageStatistics.LanguageCode(value);
+                    OnPropertyChanged(nameof(Language));
+                }
+            }
+        }
+
+        [TaskPane("CharCaseCaption", "CharCaseTooltip", null, 1, false, ControlType.ComboBox, new string[] { "Lowercase", "Titlecase", "Uppercase" })]
+        public CharCase CharacterCase
+        {
+            get
+            {
+                return _CharCase;
+            }
+            set
+            {
+                _CharCase = value;
+                OnPropertyChanged(nameof(CharacterCase));
+            }
+        }
+
+        [TaskPane("CharDirCaption", "CharDirTooltip", null, 1, false, ControlType.ComboBox, new string[] { "Forward", "Reverse" })]
+        public CharDir CharacterDirection
+        {
+            get
+            {
+                return _CharDir;
+            }
+            set
+            {
+                _CharDir = value;
+                OnPropertyChanged(nameof(CharacterDirection));
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Initialize()
+        {
+
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            EventsHelper.PropertyChanged(PropertyChanged, this, propertyName);
+        }
+
+        #endregion
+
+    }
+}
